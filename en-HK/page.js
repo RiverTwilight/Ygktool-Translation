fc089dd2de8cd84070d1dd1788dfb0f558f9c801
hsdbql <48542432+hsdbql@users.noreply.github.com>
2021-11-01 14:55:45 +0800

    Update page.js

--- en-HK/page.js
+++ en-HK/page.js
@@ -4,6 +4,19 @@ module.exports = {
 		description: "",
 	},
 	"/setting": {
-		title: "Setting",
+		title: "Settings",
+
+		menu_hitokoto_source: "Hitokoto Source",
+		menu_customize_title: "Customization",
+		menu_theme: "Color&Theme",
+
+		menu_contact_title: "Contact us",
+		menu_contact_developer: "Contact the Developer",
+
+		menu_about_title: "About",
+		menu_donation: "Donate",
+		menu_privacy: "Privacy Policy",
+		menu_group: "Join Our QQ Group",
+		menu_twitter: "Follow us on Twitter",
 	},
 };
